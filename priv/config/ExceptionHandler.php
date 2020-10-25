625157a2471b2f0b35309dc2f22df35296fdd0f1
tuomt <46566906+tuomt@users.noreply.github.com>
2020-10-25 16:59:02 +0200

    Make handle() send JSON Content-Type in the HTTP header
    The handle() method now sends the correct content-type (application/json) in the HTTP header when outputting errors/exceptions.

--- priv/config/ExceptionHandler.php
+++ priv/config/ExceptionHandler.php
@@ -5,6 +5,7 @@ error_reporting(E_ALL);
 class ExceptionHandler
 {
     static function handle($exception) {
+        header('Content-Type: application/json');
         http_response_code(500);
         echo json_encode(array("errormessage" => "Uncaught exception in the API."));
         error_log($exception->getMessage());
